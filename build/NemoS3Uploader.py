b3b4fe06547c439b6d1005a83c51d93335c629be
EE-Yat-Ho <enough6157@naver.com>
2021-03-14 16:18:28 +0900

    [pyh] 네모 빌드파일 파이썬 리셋

--- build/NemoS3Uploader.py
+++ build/NemoS3Uploader.py
@@ -32,8 +32,8 @@ def modifyManifest(manifestFileDir, date):
 def uploadFile_s3_bucket(date):
     client = boto3.client(
         's3',  # 사용할 서비스 이름, ec2이면 'ec2', s3이면 's3', dynamodb이면 'dynamodb'
-        aws_access_key_id="AKIARKPJJICTH6NNDD7K",         # 액세스 ID
-        aws_secret_access_key="A3d0iWDGtKMkVKdDzkaAYs8RmQMieS8lkFdsJ0Cf"  # 비밀 엑세스 키
+        aws_access_key_id="",     # 액세스 ID
+        aws_secret_access_key=""  # 비밀 엑세스 키
     )
     
     # 젠킨스의 manifest 경로 찾기
